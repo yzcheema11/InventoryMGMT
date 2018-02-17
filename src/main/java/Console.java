aa2944084f95fb1614e1bf0e08c728b079a8a9f1
Z. Cheema <32852915+yzcheema11@users.noreply.github.com>
2018-02-17 14:15:10 -0500

    Added parsing

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,12 +1,31 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Console {
 
     Scanner input = new Scanner(System.in);
 
-    public void takeInInput(){
+    String commands = input.nextLine();
 
+    public String[] parseInput(){
+
+        String inventory[] = commands.split(" ");
+
+        System.out.println(Arrays.toString(inventory));
+        System.out.println(inventory[2]);
+
+        return inventory;
+    }
+
+    public String parsedCommand(){
+        String commandDirection = "";
+
+        switch (commandDirection){
+
+
+        }
     }
 
 
 }
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
 public class Main {
 
+
+    
     public static void main(String[] args) {
+        Console console = new Console();
+
         System.out.println("Hello World");
+        
+        console.parseInput();
+        
     }
 
 
