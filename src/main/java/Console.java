4a586b30a0951fd987e709372ee23f019b01a8b3
Z. Cheema <32852915+yzcheema11@users.noreply.github.com>
2018-02-16 19:51:09 -0500

    Setting up Git

---
+++ src/main/java/Console.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Console {
+
+    Scanner input = new Scanner(System.in);
+
+    public void takeInInput(){
+
+    }
+
+
+}
---
+++ src/main/java/Crud.java
@@ -0,0 +1,22 @@
+import java.util.TreeMap;
+
+public class Crud {
+
+    public void createProduct(String itemName, Double costPrice, Double sellingPrice, int quantity){
+
+
+    }
+
+    public void deleteProdcut(String itemName){
+
+    }
+
+    public void updateBuy (String itemName, int quantity){
+
+    }
+
+    public void updateSell (String itemName, int quantity){
+
+    }
+
+}
